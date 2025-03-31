bbed044b7bc3c653a46c1b548f0fbf16fc47c795
HelenaKettle78 <helenakettle78@gmail.com>
2025-03-31 15:16:35 -0400

    Update helenabot_app.py

--- helenabot_app.py
+++ helenabot_app.py
@@ -37,7 +37,7 @@ def vote(email, password):
     """Attempts to vote using the given email and password."""
     try:
         wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div/div[1]/button'))).click()
-        time.sleep(2)
+        time.sleep(1)
     except:
         print(f"Accedi button not found for {email}, skipping login.")
         return False
@@ -60,7 +60,7 @@ def vote(email, password):
 
     try:
         wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div[2]/div[2]/div/div/form/div[2]/div[3]/div[6]/input'))).click()
-        time.sleep(2)
+        time.sleep(1)
     except:
         print(f"Invia button not found for {email}, skipping...")
         return False
@@ -86,7 +86,7 @@ def vote(email, password):
     try:
         logout_button = wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div/header/div/button')))
         logout_button.click()
-        time.sleep(2)
+        time.sleep(1)
     except:
         print("Logout button not found, continuing anyway.")
 
